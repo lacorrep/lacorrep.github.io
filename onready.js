134ff15650801b219f2278f0a6ad7364e49a340b
selakant <35936610+selakant@users.noreply.github.com>
2019-07-11 23:38:52 +0200

    Intro and research
    Responsive CSS. TODO projects, resources, contact form.

---
+++ onready.js
@@ -0,0 +1,9 @@
+function showContactForm()
+{
+	/*
+	document.getElementById("contact-form").classList.toggle('dropdown');
+	window.setTimeout(function(){
+		window.scrollTo(window.scrollX,window.scrollMaxY)
+	},800);
+	*/
+}
